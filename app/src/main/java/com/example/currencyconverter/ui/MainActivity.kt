d9a87d3f86b70fb137867b220386bfdeda51d4f8
Kris <blackphoenix134@mail.ru>
2025-06-22 22:04:27 +0300

    feat: add Compose UI to MainActivity
    - Implement setContent with NavGraph in MainActivity for Compose UI.
    - Use ComponentActivity instead of FragmentActivity for Compose support.

--- app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.currencyconverter.ui
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.fragment.app.FragmentActivity
+import com.example.currencyconverter.ui.navigation.NavGraph
+import com.example.currencyconverter.ui.theme.CurrencyConverterTheme
 
-class MainActivity : FragmentActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
+        setContent {
+            CurrencyConverterTheme {
+                NavGraph()
+            }
+        }
     }
 }
